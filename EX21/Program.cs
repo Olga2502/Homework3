d031bcaf035b6eba1752a1246922980fb24f161a
Olga <101519229+Olga2502@users.noreply.github.com>
2023-02-16 00:04:12 +0300

    Окончательное решение 21 задачи

--- EX21/Program.cs
+++ EX21/Program.cs
@@ -20,3 +20,10 @@ int y2 = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Введите координату второй точки z2:");
 int z2 = int.Parse(Console.ReadLine()!);
+
+double yy = Math.Pow((y1 - y2), 2);
+double zz = Math.Pow((z1 -z2), 2);
+double xx = Math.Pow((x1 - x2), 2);
+double a = Math.Sqrt(yy + xx + zz);
+//a = Math.Round(a, 2);
+Console.WriteLine($"Расстояние между точками равно:  {a:f2} ");
